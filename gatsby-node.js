8bcbdaae51f980ca3eb1f35541749d2ddd4b788e
Uriel Benítez Medina <urielbenitez96@hotmail.com>
2021-10-19 23:09:45 -0500

    get series from datocms

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,13 +17,24 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       }
     }
   `)
-  if (response.erros || response_documental.errors) {
+  const response_serie = await graphql(`
+    query {
+      allDatoCmsSerie {
+        nodes {
+          originalId
+        }
+      }
+    }
+  `)
+  if (response.erros || response_documental.errors || response_serie.errors) {
     reporter.panic("No hubo resultados")
   }
 
   //si hay resultados
   const movies = response.data.allDatoCmsMovie.nodes
   const documentales = response_documental.data.allDatoCmsDocumental.nodes
+  const series = response_serie.data.allDatoCmsSerie.nodes
+  // si hay resultado de peliculas
   movies.forEach(movie => {
     actions.createPage({
       path: `movies/movie=${movie.originalId}`,
@@ -40,6 +51,16 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       component: require.resolve("./src/components/Documental.jsx"),
       content: {
         originalId: documental.originalId,
+      },
+    })
+  })
+  // si hay resultados de series
+  series.forEach(serie => {
+    actions.createPage({
+      path: `movies/serie=${serie.originalId}`,
+      component: require.resolve("./src/components/Serie.jsx"),
+      content: {
+        originalId: serie.originalId,
       },
     })
   })
